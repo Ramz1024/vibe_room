f9f9480c4deb07f9615ddf0eaa35e317873ba740
spkchalla <shanmukhapadmakumar.challa@gmail.com>
2025-06-19 17:19:24 +0530

    added heading in login and signup pages

--- src/login.jsx
+++ src/login.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function login() {
   return (
     <div>
-      <h2>Login</h2>
+      <h2>This is Login Page</h2>
 
       <input type="text" 
         placeholder='Enter your email or username'
